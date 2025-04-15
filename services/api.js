76d30ab851cc88f14d943a4197e318dd49a78261
Lívia Neto <liviamneto@gmail.com>
2025-04-14 22:03:19 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://sspupurkokggvyaexrrj.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzcHVwdXJrb2tnZ3Z5YWV4cnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMzYwNTIsImV4cCI6MjA1OTkxMjA1Mn0.GXnAfW_wymfrEw2d1sT9pNeNP5PIhmU8DSsAJ00uz6g",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzcHVwdXJrb2tnZ3Z5YWV4cnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMzYwNTIsImV4cCI6MjA1OTkxMjA1Mn0.GXnAfW_wymfrEw2d1sT9pNeNP5PIhmU8DSsAJ00uz6g"
+        apikey: "ey...z6g",
+        authorization: "Bearer ey...z6g"
     }
 })
